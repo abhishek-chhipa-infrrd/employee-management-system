a282e15933d51db89a7fc83f7f6f0d74e9eca1ee
abhishekinfrrd <112868050+abhishekinfrrd@users.noreply.github.com>
2023-03-13 01:29:17 +0530

    basic employee management system fullstack app

---
+++ employee-system-api/src/main/java/com/springboot/Employee/entity/EmployeeEntity.java
@@ -0,0 +1,23 @@
+package com.springboot.Employee.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+
+}
